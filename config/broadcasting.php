0c6edb7d9236bb7e6b7ad0b3dd748bf470831489
Nicolás Navarro <nicolas@myseocompany.co>
2025-06-05 10:32:43 -0500

    habilitar en .env el pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -47,7 +47,7 @@ return [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
-/*
+
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -65,7 +65,7 @@ return [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
-*/
+
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
